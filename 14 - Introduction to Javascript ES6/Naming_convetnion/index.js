ab2728d3ab13ca66873e06eda0c84e2538926898
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-18 19:12:51 +0200

    Test 2
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

---
+++ 14 - Introduction to Javascript ES6/Naming_convetnion/index.js
@@ -0,0 +1,24 @@
+// Question 1:
+// Which variable name is not a valid in Javascript?
+
+// var myage = 12;
+
+// var my age = 12
+
+// var myAge = 12
+
+// var my_age = 12
+
+// Submit
+// Question 2:
+// Which is the best name for a Javascript variable that will represent student 1's score?
+
+// 1studentScore
+
+// s1Score
+
+// studentonescore
+
+// student1Score
+
+// Submit
