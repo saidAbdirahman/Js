d6ac5fca1ec5a467ed9dabd74943a1ac1c1951f7
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-20 05:32:31 +0200

    Test 5
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

--- 14 - Introduction to Javascript ES6/length/index.js
+++ 14 - Introduction to Javascript ES6/length/index.js
@@ -1,2 +1,6 @@
-var name = "said abdirahman said"
-console.log(name.length)
+// var name = "said abdirahman said"
+// console.log(name.length)
+
+var tweet =  prompt("compose your tweet")
+var tweetCount = tweet.length
+console.log("You have written " + tweetCount +  " characters, you have " + (140 - tweetCount) + " characters remaining")
