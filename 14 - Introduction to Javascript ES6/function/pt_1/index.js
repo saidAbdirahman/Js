5f5a75ae521c2c0e0963ed71241ce299448703ed
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-21 06:26:06 +0200

    test 10
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

---
+++ 14 - Introduction to Javascript ES6/function/pt_1/index.js
@@ -0,0 +1,18 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+function main(){
+    //your code here
+    move();
+    move();
+    move();
+    move();
+    turnLeft();
+    move();
+    move();
+    move();
+    move();
+ }
+ 
---
+++ 14 - Introduction to Javascript ES6/function/pt_2/index.js
@@ -0,0 +1,28 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+function main(){
+    //your code here
+    putBeeper();
+    turnLeft1();
+    putBeeper();
+    turnRight1();
+    putBeeper();
+    turnLeft1();
+    putBeeper();
+    turnRight1();
+    putBeeper();
+    
+ }
+ function turnLeft1(){
+    move();
+    turnLeft();
+    move();
+ }
+ function turnRight1(){
+    move();
+    turnRight();
+    move();
+ }
