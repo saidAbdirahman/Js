f001632c8f9af8cc45b464770a1662472463c133
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-25 16:38:39 +0200

    comparators
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

--- 15 - Intermediate Javascript/Random/index.js
+++ 15 - Intermediate Javascript/Random/index.js
@@ -6,7 +6,9 @@ loveScore = Math.floor(loveScore) + 1;
 
 if(loveScore >= 70){
     alert('Your love score is ' + loveScore + '%' + ' You love each other like qaays love layla')
-}else{
+}else if(loveScore > 30 && loveScore < 70){
     alert('Your love score is ' + loveScore + '%' )
-
+}
+else{
+    alert('Your love score is ' + loveScore + '%' + 'You go together like all and water' )
 }
