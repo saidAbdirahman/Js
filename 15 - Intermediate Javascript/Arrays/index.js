328b51f71c2107d9d6393956b15e7c7dae596499
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-25 17:14:34 +0200

    Arrays include
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

---
+++ 15 - Intermediate Javascript/Arrays/index.js
@@ -0,0 +1,8 @@
+var guestList = ['said', 'ibra', 'yasiin', 'abdalla']
+var name = prompt('enter your name')
+if(guestList.includes(name)){
+    alert('welcome')
+}else{
+    
+    alert('sorry you are not in the list')
+}
