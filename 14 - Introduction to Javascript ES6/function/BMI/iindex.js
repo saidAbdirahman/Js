e7312377b1890e15acfe82114ae63270f0e5f72b
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-25 16:16:10 +0200

    Random love percent
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

--- 14 - Introduction to Javascript ES6/function/BMI/iindex.js
+++ 14 - Introduction to Javascript ES6/function/BMI/iindex.js
@@ -5,5 +5,5 @@ function bmiCalc(weight,height){
     var bmiCal = Math.round(weight/(Math.pow(height,2)))
     return bmiCal
 }
-var bmire = bmiCalc(65,1.8)
-console.log('Your bmi is ' + bmire)
+var bmi = bmiCalc(65,1.8)
+console.log('Your bmi is ' + bmi)
---
+++ 15 - Intermediate Javascript/Random/index.js
@@ -0,0 +1,6 @@
+var man = prompt('enter man name')
+var women = prompt('enter women name')
+var loveScore = Math.random();
+loveScore = loveScore * 100
+loveScore = Math.floor(loveScore) + 1;
+alert(loveScore + '%')
