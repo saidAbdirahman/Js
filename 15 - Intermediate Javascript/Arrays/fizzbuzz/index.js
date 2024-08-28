f9836a66aa5b51650c17fdc360457bb694f6da6b
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-28 18:48:19 +0200

    FizzBuzz challenge while loop
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

--- 15 - Intermediate Javascript/Arrays/fizzbuzz/index.js
+++ 15 - Intermediate Javascript/Arrays/fizzbuzz/index.js
@@ -14,22 +14,22 @@
 var output = [];
 var count = 1
 function fizzBuzz(){
+   while(count<=100){
     if(count % 3 == 0){
-           output.push('fizz') 
-            }else if(count % 5 == 0){
-           output.push('Buzz') 
-            }else if(count % 3 == 0 &&i % 5 == 0 ){
-                output.push('fizzBuzz') 
-            }
-            else{
-                output.push(count)
-            }
-    count++
-   
-    console.log(output)
+        output.push('fizz') 
+         }else if(count % 5 == 0){
+        output.push('Buzz') 
+         }else if(count % 3 == 0 &&i % 5 == 0 ){
+             output.push('fizzBuzz') 
+         }
+         else{
+             output.push(count)
+         }
+ count++
+   }
+   console.log(output)
 }
 fizzBuzz()
-fizzBuzz()
-fizzBuzz()
+
 
 
