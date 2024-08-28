324886459fc08323510c54fc9fd805ac3865c782
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-28 18:36:44 +0200

    FizzBuzz challenge
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

---
+++ 15 - Intermediate Javascript/Arrays/fizzbuzz/index.js
@@ -0,0 +1,35 @@
+// for(let i = 1; i<=100; i++){
+//     if(i % 3 == 0){
+//         document.write("Fizz" + '<br>') 
+//     }else if(i % 5 == 0){
+//         document.write('Buzz' + '<br>') 
+//     }else if(i % 3 == 0 &&i % 5 == 0 ){
+//         document.write("Fizzbuzz" + '<br>') 
+//     }
+//     else{
+//         document.write(i+ '<br>')
+//     }
+// }
+
+var output = [];
+var count = 1
+function fizzBuzz(){
+    if(count % 3 == 0){
+           output.push('fizz') 
+            }else if(count % 5 == 0){
+           output.push('Buzz') 
+            }else if(count % 3 == 0 &&i % 5 == 0 ){
+                output.push('fizzBuzz') 
+            }
+            else{
+                output.push(count)
+            }
+    count++
+   
+    console.log(output)
+}
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+
+
