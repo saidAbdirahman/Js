bd4df6e5b31baf4af4356f519cd9fa40b52fc736
saidAbdirahman <saidqaaje11@gmail.com>
2024-09-05 07:14:16 +0200

    calculator
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

---
+++ Calculator/index.js
@@ -0,0 +1,17 @@
+function add (num1,num2){
+    return num1 + num2
+}
+function subtract (num1,num2){
+    return num1 - num2
+}
+function multiply (num1,num2){
+    return num1 * num2
+}
+function division (num1,num2){
+    return num1 / num2
+}
+
+function calculator (num1,num2,operator){
+    return operator(num1,num2)
+}
+console.log(calculator(6,2,add))
